f97ab78eb0820ba683b3a717534a8866814ea32f
Guiwoo <park.guiwoo@hotmail.com>
2022-04-19 20:31:54 +0900

    graphql how to handle mutation

---
+++ README.md
@@ -0,0 +1,12 @@
+# WorkOut Routine Project
+
+    - Go, GraphQL, PostgreSQL
+
+## Why GraphQL ?
+
+- [Click here Explain why graphql](howtographql.com/basics/1-graphql-is-the-better-rest/)
+
+## Which GrpahQL Package are you gonna use ?
+
+- 1. Go grpahQL
+  - don't like it becaz not supporting the webpage
--- main.go
+++ main.go
@@ -39,6 +39,8 @@ func populate() []Tutorial {
 	return tutorials
 }
 
+var tutorials = populate()
+
 var commentType = graphql.NewObject(
 	graphql.ObjectConfig{
 		Name: "Comment",
@@ -84,18 +86,64 @@ var tutorialType = graphql.NewObject(
 	},
 )
 
+var mutationType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "Mutation",
+	Fields: graphql.Fields{
+		"create": &graphql.Field{
+			Type:        tutorialType,
+			Description: "Create a new Tutorial",
+			Args: graphql.FieldConfigArgument{
+				"title": &graphql.ArgumentConfig{
+					Type: graphql.NewNonNull(graphql.String),
+				},
+			},
+			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+				tutorial := Tutorial{
+					Title: p.Args["title"].(string),
+				}
+				tutorials = append(tutorials, tutorial)
+				return tutorial, nil
+			},
+		},
+	},
+})
+
 func main() {
 	// Schema
 	fields := graphql.Fields{
-		"hello": &graphql.Field{
-			Type: graphql.String,
+		"tutorial": &graphql.Field{
+			Type:        tutorialType,
+			Description: "Get Tutorial By ID",
+			Args: graphql.FieldConfigArgument{
+				"id": &graphql.ArgumentConfig{
+					Type: graphql.Int,
+				},
+			},
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return "world", nil
+				id, ok := p.Args["id"].(int)
+				if ok {
+					for _, tutorial := range tutorials {
+						if int(tutorial.ID) == id {
+							return tutorial, nil
+						}
+					}
+				}
+				return nil, nil
+			},
+		},
+		"list": &graphql.Field{
+			Type:        graphql.NewList(tutorialType),
+			Description: "Get Tutorial List",
+			Resolve: func(params graphql.ResolveParams) (interface{}, error) {
+				return tutorials, nil
 			},
 		},
 	}
 	rootQuery := graphql.ObjectConfig{Name: "RootQuery", Fields: fields}
-	schemaConfig := graphql.SchemaConfig{Query: graphql.NewObject(rootQuery)}
+	schemaConfig := graphql.SchemaConfig{
+		Query:    graphql.NewObject(rootQuery),
+		Mutation: mutationType,
+	}
 	schema, err := graphql.NewSchema(schemaConfig)
 	if err != nil {
 		log.Fatalf("failed to create new schema, error: %v", err)
@@ -103,8 +151,10 @@ func main() {
 
 	// Query
 	query := `
-		{
-			hello
+		mutation {
+			create(title:"Hello World"){
+				title
+			}
 		}
 	`
 	params := graphql.Params{Schema: schema, RequestString: query}
@@ -114,4 +164,20 @@ func main() {
 	}
 	rJSON, _ := json.Marshal(r)
 	fmt.Printf("%s \n", rJSON) // {"data":{"hello":"world"}}
+
+	query = `
+		{
+			list {
+				id
+				title
+			}
+		}
+	`
+	params = graphql.Params{Schema: schema, RequestString: query}
+	r = graphql.Do(params)
+	if len(r.Errors) > 0 {
+		log.Fatalf("failed to execute graphql operation, errors: %+v", r.Errors)
+	}
+	rJSON, _ = json.Marshal(r)
+	fmt.Printf("%s \n", rJSON)
 }
